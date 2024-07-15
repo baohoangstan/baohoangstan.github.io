48ff0a4f23672c75f1ad72335cbf6f65ad058c4d
Bao Hoang <baohoangstan@gmail.com>
2024-07-14 21:17:31 -0700

    update flows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,11 +17,12 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
+          Baochill's blog
+          {/* <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
             Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          </Link> */}
         </div>
       </div>
     </header>
