809d0ef49317c100d1daf9594c2b0282c0adec9d
Bao Hoang <baohoangstan@gmail.com>
2025-09-15 10:38:25 +0700

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -172,7 +172,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} baochill. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} baohoangstan`,
     },
     prism: {
       theme: prismThemes.github,
